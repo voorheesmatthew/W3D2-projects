0da91de30a2a5be72ee6a838df78520ccf49a1e9
Matt <voorhees.matthew@gmail.com>
2018-09-04 16:13:13 -0700

    Build classes and ::all and ::find_by methods for questions.rb file

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -2,27 +2,27 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
-  fname TEXT NOT NULL,
-  lname TEXT NOT NULL,
-)
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
-  title TEXT NOT NULL,
+  title VARCHAR(255) NOT NULL,
   body TEXT NOT NULL,
   user_id INTEGER NOT NULL,
   
   FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
   
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
   
 
 CREATE TABLE replies (
@@ -32,17 +32,27 @@ CREATE TABLE replies (
   parent_id INTEGER,
   body TEXT NOT NULL,
   
-  FOREIGN KEY (question_id) REFERENCES questions(id)
-  FOREIGN KEY (parent_id) REFERENCES replies(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
   FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
   
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Faith', 'Tan'), ('Matt', 'Voorhees');
+  
+INSERT INTO
+  questions (title, body, user_id)
+VALUES
+  ('SQL????', 'What is?', (SELECT id FROM users WHERE users.fname = 'Faith')),
+  ('Rails???!', 'Also, What is?', (SELECT id FROM users WHERE users.fname = 'Matt'));
---
+++ aa_questions/questions.rb
@@ -0,0 +1,126 @@
+require 'byebug'
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+  
+  def initialize 
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end 
+end 
+
+class User
+  attr_accessor :fname, :lname
+  
+  def self.all 
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end 
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE users.id = ?", id)
+    User.new(data.first)
+  end
+  
+  def self.find_by_name(fname, lname)
+    options = { 'fname' => fname, 'lname' => lname }
+    User.new(options)
+  end 
+  
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end 
+
+class Question 
+  attr_accessor :title, :body, :user_id
+  
+  def self.all 
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end 
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions WHERE questions.id = ?", id)
+    Question.new(data.first)
+  end
+  
+  def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions WHERE questions.user_id = ?", user_id)
+    Question.new(data.first)
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @user_id = options['user_id']
+  end
+end 
+
+class QuestionFollow 
+  attr_accessor :user_id, :question_id
+  
+  def self.all 
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+    data.map { |datum| QuestionFollow.new(datum) }
+  end 
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows WHERE question_follows.id = ?", id)
+    QuestionFollow.new(data.first)
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+  
+end
+
+class Reply
+  attr_accessor :user_id, :question_id, :parent_id, :body
+  
+  def self.all 
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+    data.map { |datum| Reply.new(datum) }
+  end 
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies WHERE replies.id = ?", id)
+    Reply.new(data.first)
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+    @parent_id = options['parent_id']
+    @body = options['body']
+  end
+end 
+
+class QuestionLike
+  attr_accessor :user_id, :question_id
+  
+  def self.all 
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes")
+    data.map { |datum| QuestionLike.new(datum) }
+  end 
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes WHERE question_likes.id = ?", id)
+    QuestionLike.new(data.first)
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+  end
+end
