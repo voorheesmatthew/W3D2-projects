f654c2628eb77457e0a8b35aceab539da96a8d34
Matt <voorhees.matthew@gmail.com>
2018-09-04 12:16:40 -0700

    Create tables for db

---
+++ aa_questions/import_db.sql
@@ -0,0 +1,48 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL,
+)
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+  
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  body TEXT NOT NULL,
+  
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
